b2d81fe3cd0d544e8f1f4b0851456cf03aa64068
Rencie Bautista <rencie.bautista@yahoo.com>
2015-09-28 23:14:39 +0800

    limit return

--- resources/views/iot/index.blade.php
+++ resources/views/iot/index.blade.php
@@ -22,7 +22,7 @@
               <!-- LINE CHART -->
               <div class="box box-info">
                 <div class="box-header with-border">
-                  <h3 class="box-title">As of 24 Hours</h3>
+                  <h3 class="box-title">200 latest records</h3>
                   <div class="box-tools pull-right">
                     <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                     <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
